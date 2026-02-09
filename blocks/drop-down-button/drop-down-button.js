572cd715c341b1b0eecfb8b96e0822f061726f2c
deepz4work <deepz4work@gmail.com>
2026-02-09 14:55:45 +0530

    dropdown.js

--- blocks/drop-down-button/drop-down-button.js
+++ blocks/drop-down-button/drop-down-button.js
@@ -1,6 +1,9 @@
 
 
 export default function decorate(block) {
-    
+      const button1= document.createElement('button');
+      [...block.children].forEach((row) => {
+        debugger
+      });
 
 }
